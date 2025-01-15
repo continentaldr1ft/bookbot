f0247e6329ccf7216a4f689b3a713ec6ed3575e2
continentaldr1ft <ckissinger@live.com>
2025-01-15 15:23:43 -0600

    word count

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
-    with open("books/frankenstein.txt") as book:
-        book_contents = book.read()
-        print(book_contents)
+    with open("books/frankenstein.txt") as file:
+        file_contents = file.read()
+        #print(file_contents)
+    word_count = 0
+    words = file_contents.split()
+    for word in words:
+        word_count += 1
+    print(word_count)
 
 main()

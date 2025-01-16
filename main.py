5e0d4b74270a2296584f2686abecac9d404732dd
continentaldr1ft <ckissinger@live.com>
2025-01-15 21:34:10 -0600

    Final version

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def main():
     path = "books/frankenstein.txt"
     text = get_book(path)
     total_count = get_words(text)
+    dictionary = count_characters(text)
+    print("--- Begin report of Book ---")
     print(f'There are {total_count} words in this document.')
+    for letter, count in sorted(dictionary.items()):
+        print(f"The '{letter}' character was found {count} times")
+    print("--- End Report ---")
 
 def get_book(path):
     with open(path) as file:
@@ -14,6 +19,17 @@ def get_words(text):
     words = text.split()
     return (len(words))
 
+def count_characters(text):
+    
+    lower_case = text.lower()
+    alphabet = {}
+    for letter in lower_case:
+        if letter.isalpha():
+            if letter not in alphabet:
+                alphabet[letter] = 1
+            else:
+                alphabet[letter] += 1
+    return alphabet
     
     
 
